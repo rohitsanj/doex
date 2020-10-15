e424cdafc40070736b70eb78d27c360580a21483
Rohit Sanjay <sanjay.rohit2@gmail.com>
2020-10-15 21:00:49 +0530

    added tests to bring up coverage

--- doex/tests/test_latin_square.py
+++ doex/tests/test_latin_square.py
@@ -1,5 +1,7 @@
 import math
 
+import pytest
+
 from ..latin_square import LatinSquare
 
 
@@ -30,3 +32,42 @@ class TestLatinSquare:
         assert math.isclose(exp.p_treatments, 0.0005, abs_tol=abs_tol)
         assert math.isclose(exp.p_rows, 0.3476, abs_tol=abs_tol)
         assert math.isclose(exp.p_columns, 0.4550, abs_tol=abs_tol)
+
+    def test_latin_square_raises_error_shape_mismatch(self):
+        with pytest.raises(ValueError):
+            LatinSquare(
+                [
+                    ["A", "B", "D", "C", "E"],
+                    ["C", "E", "A", "D", "B"],
+                    ["B", "A", "C", "E", "D"],
+                    ["D", "C", "E", "B", "A"],
+                    ["E", "D", "B", "A", "C"],
+                ],
+                [
+                    [8, 7, 1, 7, 3],
+                    [11, 2, 7, 3, 8],
+                    [4, 9, 10, 1, 5],
+                    [6, 8, 6, 6, 10],
+                    [4, 2, 3, 8, 8],
+                    [4, 2, 3, 8, 8],  # Extra row
+                ],
+            )
+
+    def test_latin_square_raises_error_treatments(self):
+        with pytest.raises(ValueError):
+            LatinSquare(
+                [
+                    ["A", "B", "D", "C", "E"],
+                    ["C", "E", "A", "D", "B"],
+                    ["B", "A", "C", "E", "D"],
+                    ["D", "C", "E", "B", "A"],
+                    ["E", "G", "B", "F", "C"],
+                ],
+                [
+                    [8, 7, 1, 7, 3],
+                    [11, 2, 7, 3, 8],
+                    [4, 9, 10, 1, 5],
+                    [6, 8, 6, 6, 10],
+                    [4, 2, 3, 8, 8],
+                ],
+            )
